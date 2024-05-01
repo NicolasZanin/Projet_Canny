be43d15087ef54ef1edaf0cb0999709d416450f9
amandinemart1 <amandine-martin@sfr.fr>
2024-05-01 14:59:22 +0200

    addition bw if in argument

--- project-gpu.py
+++ project-gpu.py
@@ -1,5 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 import argparse
+import numpy as np
+from numba import cuda
+import numba as nb
+from PIL import Image
+import sys
+import math
 
 inputImage = None
 outputImage = None
@@ -8,6 +14,38 @@ applyBw = False
 applyGaussian = False
 applySobel = False
 applyThreshold = False
+
+@cuda.jit
+def RGBToBWKernel(source, destination, offset):
+    height = source.shape[1]
+    width = source.shape[0]
+    x,y = cuda.grid(2)
+    if (x<width and y<height) :
+        r_x= (x+offset)%width
+        r_y= (y+offset)%height
+        destination[r_x,r_y]=np.uint8(0.3*source[r_x,r_y,0]+0.59*source[r_x,r_y,1]+0.11*source[r_x,r_y,2])
+
+def gpu_rgb_to_bw(image):
+    threadsperblock = (8, 8)
+    width, height = image.shape[1], image.shape[0]
+    blockspergrid_x = math.ceil(width / threadsperblock[0])
+    blockspergrid_y = math.ceil(height / threadsperblock[1])
+    blockspergrid = (blockspergrid_x, blockspergrid_y)
+
+    s_image = cuda.to_device(image)
+    d_image = cuda.device_array((image.shape[0], image.shape[1]), dtype=np.uint8)
+
+    offset_range = range(33, 1, -1)
+    for off in offset_range:
+        runs = 6
+        result = np.zeros(runs, dtype=np.float32)
+        for i in range(runs):
+            RGBToBWKernel[blockspergrid, threadsperblock](s_image, d_image, off)
+            cuda.synchronize()
+
+        output = d_image.copy_to_host()
+
+    return output
 
 def getAllArgs():
     global inputImage, outputImage, nombreThreads, applyBw, applyGaussian, applySobel, applyThreshold
@@ -43,3 +81,10 @@ if __name__ == '__main__':
     getAllArgs()
     print("ok")
     print(f"Input image : {inputImage}\nOutputImage : {outputImage}")
+    temp = Image.open(inputImage)
+    temptab = np.array(temp)
+    
+    if applyBw:
+        output = gpu_rgb_to_bw(temptab)
+        m = Image.fromarray(output)
+        m.save(outputImage)
