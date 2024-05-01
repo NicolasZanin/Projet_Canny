1609c433c4b3c63419bbb04363c9fbfa1410a1fb
amandinemart1 <amandine-martin@sfr.fr>
2024-05-01 14:13:56 +0200

    argument inputImage and outputeImage take argument into account

--- project-gpu.py
+++ project-gpu.py
@@ -10,34 +10,36 @@ applySobel = False
 applyThreshold = False
 
 def getAllArgs():
+    global inputImage, outputImage, nombreThreads, applyBw, applyGaussian, applySobel, applyThreshold
+    
     parser = argparse.ArgumentParser()
     
     parser.add_argument("--tb", type=int)
-    parser.add_argument("--bw")
-    parser.add_argument("--gauss")
-    parser.add_argument("--sobel")
-    parser.add_argument("--threshold")
+    parser.add_argument("--bw", action="store_true")
+    parser.add_argument("--gauss", action="store_true")
+    parser.add_argument("--sobel", action="store_true")
+    parser.add_argument("--threshold", action="store_true")
     parser.add_argument("inputImage", type=str)
     parser.add_argument("outputImage", type=str)
     
     args = parser.parse_args()
 
-    if (args.tb != None):
+    if args.tb is not None:
         nombreThreads = args.tb
-    if (args.bw != None):
+    if args.bw:
         applyBw = True
-    if (args.gauss != None):
+    if args.gauss:
         applyGaussian = True
-    if (args.sobel != None):
+    if args.sobel:
         applySobel = True
-    if (args.threshold != None):
+    if args.threshold:
         applyThreshold = True
-    if (args.inputImage != None):
+    if args.inputImage is not None:
         inputImage = args.inputImage
-    if (args.outputImage != None):
+    if args.outputImage is not None:
         outputImage = args.outputImage
 
 if __name__ == '__main__':
     getAllArgs()
-    print(f"{ok}")
-    print(f"Input image : {inputImage}\nOutputImage : {outputImage}")# \nNombreThreads : {nombreThreads}\nBW : {applyBw}\nGaussian : {applyGaussian}\nSobel : {applySobel}\nThreshold : {applyThreshold} ")
+    print("ok")
+    print(f"Input image : {inputImage}\nOutputImage : {outputImage}")
