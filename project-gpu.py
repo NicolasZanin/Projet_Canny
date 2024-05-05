8224fc76c17dfbf85c39e991bd7b3bbc20c5a969
NicolasZanin <nicolas.zanin@etu.univ-cotedazur.fr>
2024-05-05 12:31:42 +0200

    feature: upgrad args

--- project-gpu.py
+++ project-gpu.py
@@ -6,17 +6,11 @@ import numba as nb
 from PIL import Image
 import sys
 import math
-import time
-from timeit import default_timer as timer
 
 inputImage = None
 outputImage = None
 nombreThreads = 16
-applyBw = False
-applyGaussian = False
-applySobel = False
-applyThreshold = False
-applyHysterisis = False
+applyArgs = [True, True, True, True, True]
 threadBlockSize = (8, 8)  # Default thread block size
 
 # Calculate Blocks par grid necessary for the source image depending on threadBlockSize
@@ -40,7 +34,7 @@ def RGBToBWKernel(source, destination, offset):
         r_y = (y + offset) % height
 
         # Formula to convert color in gray
-        destination[r_x, r_y] = np.uint8(0.3 * source[r_x, r_y, 0] + 0.59 * source[r_x, r_y, 1] + 0.1 * source[r_x, r_y, 2]) 
+        destination[r_x, r_y] = np.uint8(0.3 * source[r_x, r_y, 0] + 0.59 * source[r_x, r_y, 1] + 0.1 * source[r_x, r_y, 2])
 
 # Perform the GPU Kernel BW
 def gpu_rgb_to_bw(source, threadsPerBlock, blockspergrid):
@@ -67,7 +61,7 @@ def GaussianBlurKernel(source, destination, filter):
     if x < source.shape[0] and y < source.shape[1]:
         width, height = source.shape
 
-        # Apply the convolution filter on a single pixel  
+        # Apply the convolution filter on a single pixel
         weighted_sum = 0
         normalization_factor = 0
         
@@ -113,7 +107,7 @@ def SobelKernel(source, destination, sobel_x, sobel_y, clamped_magnitude):
         width, height = source.shape[:2]
         gradient_x = 0
         gradient_y = 0
-        
+
         for indexSobelX in range(sizeSobelX[0]):
             for indexSobelY in range(sizeSobelX[1]):
                 source_x = x + indexSobelX - sizeSobelX[0] // 2
@@ -123,10 +117,10 @@ def SobelKernel(source, destination, sobel_x, sobel_y, clamped_magnitude):
                     pixel_value = source[source_x, source_y]
                 else:
                     pixel_value = source[x, y]  
-                
+
                 gradient_x += pixel_value * sobel_x[indexSobelX][indexSobelY]
                 gradient_y += pixel_value * sobel_y[indexSobelX][indexSobelY]
-        
+
         magnitude = math.sqrt(gradient_x**2 + gradient_y**2)
         magnitude = min(magnitude, clamped_magnitude)
         destination[x, y] = magnitude
@@ -135,7 +129,7 @@ def SobelKernel(source, destination, sobel_x, sobel_y, clamped_magnitude):
 def gpu_sobel(source, threadsPerBlock, blocksPerGrid):
     device_input_image = cuda.to_device(source)
     device_output_image = cuda.device_array_like(source)
-    
+
     sobel_x = np.array([[-1, 0, 1],
                         [-2, 0, 2],
                         [-1, 0, 1]])
@@ -152,7 +146,7 @@ def gpu_sobel(source, threadsPerBlock, blocksPerGrid):
 @cuda.jit
 def ThresholdKernel(source, destination, lowThreshold, highThreshold):
     x, y = cuda.grid(2)
-    
+
     if x < source.shape[0] and y < source.shape[1]:
         if source[x, y] >= highThreshold:
             destination[x, y] = 255  # Potential edge
@@ -187,12 +181,12 @@ def HysterisisKernel(source, destination):
                 for offsetY in range(-1, 2, 1):
                     source_x = offsetX + x
                     source_y = offsetY + y
-                    
+
                     if source_x >= 0 and source_y >= 0 and source_x < width and source_y < height:
                         if source[source_x, source_y] == 255:
                             destination[x, y] = 255
                             return
-            
+
             destination[x, y] = 0
 
 
@@ -205,78 +199,66 @@ def gpu_hysterisis(source, threadsPerBlock, blocksPerGrid):
     return device_output_image.copy_to_host()
 
 def getAllArgs():
-    global inputImage, outputImage, nombreThreads, applyBw, applyGaussian, applySobel, applyThreshold, applyHysterisis, threadBlockSize
-    
+    global inputImage, outputImage, nombreThreads, applyArgs, threadBlockSize
+
     parser = argparse.ArgumentParser()
-    
-    parser.add_argument("--tb", type=int)
-    parser.add_argument("--bw", action="store_true")
-    parser.add_argument("--gauss", action="store_true")
-    parser.add_argument("--sobel", action="store_true")
-    parser.add_argument("--threshold", action="store_true")
-    parser.add_argument("inputImage", type=str)
-    parser.add_argument("outputImage", type=str)
-    
+
+    parser.add_argument("--tb", type=int, help="optional size of a thread block for all operations")
+    parser.add_argument("--bw", action="store_true", help="perform only the bw_kernel")
+    parser.add_argument("--gauss", action="store_true", help="perform the bw_kernel and the gauss_kernel")
+    parser.add_argument("--sobel", action="store_true", help="perform all kernels up to sobel_kernel and write to disk the magnitude of each pixel")
+    parser.add_argument("--threshold", action="store_true", help="perform all kernels up to threshold_kernel")
+    parser.add_argument("inputImage", type=str, help="the source image")
+    parser.add_argument("outputImage", type=str, help="the destination image")
+
     args = parser.parse_args()
 
     if args.tb is not None:
         threadBlockSize = (args.tb, args.tb)
-    else:
-        threadBlockSize = (8, 8)  # Default thread block size
-
-    if (not args.bw) and (not args.gauss) and (not args.sobel) and (not args.threshold):
-        applyBw = True
-        applyGaussian = True
-        applySobel = True
-        applyThreshold = True
-        applyHysterisis = True
-    else:
-        if args.bw:
-            applyBw = True
-        if args.gauss:
-            applyBw = True
-            applyGaussian = True
-        if args.sobel:
-            applyBw = True
-            applyGaussian = True
-            applySobel = True
-        if args.threshold:
-            applyBw = True
-            applyGaussian = True
-            applySobel = True
-            applyThreshold = True
+    if args.bw:
+        applyArgs[1:] = [False, False, False, False]
+    if args.gauss:
+        applyArgs[2:] = [False, False, False]
+    if args.sobel:
+        applyArgs[3:] = [False, False]
+    if args.threshold:
+        applyArgs[4] = False
     
     if args.inputImage is not None:
         inputImage = args.inputImage
+
     if args.outputImage is not None:
         outputImage = args.outputImage
 
 if __name__ == '__main__':
     getAllArgs()
-    print(f"Input image : {inputImage}\nOutputImage : {outputImage}")
-    
     openedImage = Image.open(inputImage)
     npArrayImage = np.array(openedImage)
     blocksPerGrid = calcultateBlocksPerGrid(npArrayImage)
-    
-    if applyBw:         
+
+    # Perform BW Kernel
+    if applyArgs[0]:
         output_bw = gpu_rgb_to_bw(npArrayImage, threadBlockSize, blocksPerGrid)
         npArrayImage = output_bw
 
-    if applyGaussian:
+    # Perform Gauss Kernel
+    if applyArgs[1]:
         output_gauss = gpu_gaussian_blur(npArrayImage, threadBlockSize, blocksPerGrid)
         npArrayImage = output_gauss
 
-    if applySobel:
+    # Perform Sobel Kernel
+    if applyArgs[2]:
         magnitude = gpu_sobel(npArrayImage, threadBlockSize, blocksPerGrid)
         # You can use magnitude and angle arrays for further processing or visualization
         npArrayImage = magnitude.astype(np.uint8)
-            
-    if applyThreshold:
+
+    # Perform Threshold Kernel    
+    if applyArgs[3]:
         thresholded_image = gpu_threshold(npArrayImage, threadBlockSize, blocksPerGrid)
         npArrayImage = thresholded_image
-    
-    if applyHysterisis:
+
+    # Perform Hysterisis Kernel
+    if applyArgs[4]:
         hysterisis = gpu_hysterisis(npArrayImage, threadBlockSize, blocksPerGrid)
         npArrayImage = hysterisis
 
